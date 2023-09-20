eef61e129d7798c8595ceabc23b891e2a41859e4
Noor Mudassar <noormudassar97@gmail.com>
2023-09-20 12:35:45 -0400

    entity created for users

---
+++ src/main/java/com/devmountain/capstone/entities/User.java
@@ -0,0 +1,76 @@
+package com.devmountain.capstone.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table
+public class User {
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Column(unique = true)
+    private String firstname;
+
+    public Long getId() {
+        return id;
+    }
+
+    public User() {
+    }
+
+    public User(Long id, String firstname, String lastname, String email, String password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Column(unique = true)
+    private String lastname;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String password;
+}
