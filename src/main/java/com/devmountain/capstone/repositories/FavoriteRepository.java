c2d5bbf29897e3c04f0a34bb107696fba30826cf
Noor Mudassar <noormudassar97@gmail.com>
2023-09-21 20:14:38 -0400

    set up repository layers for user, jewelry, favorite

---
+++ src/main/java/com/devmountain/capstone/repositories/FavoriteRepository.java
@@ -0,0 +1,9 @@
+package com.devmountain.capstone.repositories;
+
+import com.devmountain.capstone.entities.Favorite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+}
