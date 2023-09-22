01012e840ad0664f04d527cb2bff03d75a1e08f8
Noor Mudassar <noormudassar97@gmail.com>
2023-09-21 22:57:33 -0400

    started jewelry service layer

--- src/main/java/com/devmountain/capstone/repositories/JewelryRepository.java
+++ src/main/java/com/devmountain/capstone/repositories/JewelryRepository.java
@@ -1,9 +1,13 @@
 package com.devmountain.capstone.repositories;
 
 import com.devmountain.capstone.entities.Jewelry;
+import com.devmountain.capstone.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JewelryRepository extends JpaRepository<Jewelry, Long> {
+    List<Jewelry> findAllUsersEquals(User user);
 }
--- src/main/java/com/devmountain/capstone/services/JewelryServiceImpl.java
+++ src/main/java/com/devmountain/capstone/services/JewelryServiceImpl.java
@@ -1,4 +1,40 @@
 package com.devmountain.capstone.services;
 
+import com.devmountain.capstone.dtos.JewelryDto;
+import com.devmountain.capstone.entities.Jewelry;
+import com.devmountain.capstone.entities.User;
+import com.devmountain.capstone.repositories.JewelryRepository;
+import com.devmountain.capstone.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class JewelryServiceImpl {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JewelryRepository jewelryRepository;
+
+    @Override
+    public List<JewelryDto> getAllJewelryByUserId(Long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            List<Jewelry> jewelryList = jewelryRepository.findAllUsersEquals(userOptional.get());
+            return jewelryList.stream().map(jewelry -> new JewelryDto(jewelry)).collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+    @Override
+    public Optional<JewelryDto> getJewelryById(Long jewelryId) {
+        Optional<Jewelry> jewelryOptional = jewelryRepository.findById(jewelryId);
+        if (jewelryOptional.isPresent()) {
+            return Optional.of(new JewelryDto(jewelryOptional.get()));
+        }
+        return Optional.empty();
+    }
 }
